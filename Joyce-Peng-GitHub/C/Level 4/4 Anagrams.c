85dcb26e80d9a909f705c8e8e7e7650cdeed4a8e
Joyce <1071060893@qq.com>
2024-12-18 19:55:37 +0800

    Finished.

---
+++ Joyce-Peng-GitHub/C/Level 4/4 Anagrams.c
@@ -0,0 +1,27 @@
+#include <memory.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#define N 100000
+
+char s[N], t[N];
+size_t cnt[2][256];
+
+void count_chars(const char *s, const char *t) {
+	for (; *s; ++cnt[0][*(s++)])
+		;
+	for (; *t; ++cnt[1][*(t++)])
+		;
+}
+
+int main(void) {
+	gets(s);
+	gets(t);
+	count_chars(s, t);
+	printf("Those strings are ");
+	if (memcmp(cnt[0], cnt[1], 256 * sizeof(size_t))) {
+		printf("not ");
+	}
+	puts("anagrams.");
+	return 0;
+}
