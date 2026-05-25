8925587126ff43dc7a128b58e48ec7b57bb494e4
Joyce <1071060893@qq.com>
2026-05-25 13:31:43 +0800

    Change to a non-divide-and-conquer algorithm.

--- Joyce-Peng-GitHub/Computation Theory and Algorithm Analysis/3.15-3.21/4. Quoit.cpp
+++ Joyce-Peng-GitHub/Computation Theory and Algorithm Analysis/3.15-3.21/4. Quoit.cpp
@@ -19,6 +19,10 @@ inline void redirectIo() {
 
 struct Point {
 	double x, y;
+
+	bool operator==(const Point &other) const {
+		return (this->x == other.x && this->y == other.y);
+	}
 };
 
 constexpr size_t N = 1e5;
@@ -30,45 +34,41 @@ inline void preprocess() {}
 
 inline double square(double x) { return x * x; }
 
-void divideAndConquer(size_t beg, size_t end) {
-	if (beg == end || beg + 1 == end) return;
-	assert(beg < end);
-
-	size_t mid = beg + ((end - beg) >> 1);
-	auto mid_x = points[mid].x;
-	divideAndConquer(beg, mid);
-	divideAndConquer(mid, end);
-
-	std::inplace_merge(points.begin() + beg, points.begin() + mid, points.begin() + end,
-					   [](const Point &lhs, const Point &rhs) { return (lhs.y < rhs.y); });
-
-	std::vector<Point> cands;
-	for (size_t i = beg; i < end; ++i) {
-		if (square(points[i].x - mid_x) < ans) {
-			cands.push_back(points[i]);
-		}
-	}
-	for (size_t i = 0; i < cands.size(); ++i) {
-		for (size_t j = i + 1;
-			 j < cands.size() && square(cands[j].y - cands[i].y) < ans;
-			 ++j) {
-			auto dist = square(cands[j].x - cands[i].x) + square(cands[j].y - cands[i].y);
-			if (dist < ans) ans = dist;
-		}
-	}
-}
-
 inline bool solve() {
+	constexpr double EPS = 1e-6;
+
 	std::cin >> n;
 	if (!n) return false;
 	for (size_t i = 0; i < n; ++i) {
 		std::cin >> points[i].x >> points[i].y;
 	}
 	std::sort(points.begin(), points.begin() + n,
-			  [](const Point &lhs, const Point &rhs) { return (lhs.x < rhs.x); });
-	ans = INFINITY;
-	divideAndConquer(0, n);
+			  [](const Point &lhs, const Point &rhs) {
+				  return (lhs.x < rhs.x);
+			  });
+	if (std::unique(points.begin(), points.begin() + n) != points.begin() + n) {
+		std::cout << "0.00\n";
+		return true;
+	}
+
+	double ans = INFINITY;
+	std::set<std::pair<double, size_t>> st;
+	for (size_t i = 0, j = 0; i < n; ++i) {
+		for (; j < i && square(points[i].x - points[j].x) >= ans; ++j) {
+			st.erase(std::make_pair(points[j].y, j));
+		}
+		for (auto iter = st.lower_bound(std::make_pair(points[i].y - std::sqrt(ans) - EPS, size_t(0)));
+			 iter != st.end() && square(iter->first - points[i].y) <= ans + EPS;
+			 ++iter) {
+			auto dist = square(points[iter->second].x - points[i].x) +
+						square(points[iter->second].y - points[i].y);
+			if (dist < ans) ans = dist;
+		}
+		st.insert({points[i].y, i});
+	}
+
 	std::cout << std::fixed << std::setprecision(2) << (std::sqrt(ans) / 2) << '\n';
+
 	return true;
 }
 
